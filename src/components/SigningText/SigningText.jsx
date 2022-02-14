0a731bb71c24842da649babd69ec2e7978a1ab2d
Ankit Kushwah <74912803+Ankitkush1997@users.noreply.github.com>
2022-02-14 21:36:04 +0530

    sadsad

--- src/components/SigningText/SigningText.jsx
+++ src/components/SigningText/SigningText.jsx
@@ -3,10 +3,21 @@ import "./SigningText.css";
 
 const SigningText = () => {
   return (
-    <div className="content">
-      
-      <h2 className="text" data-text="STATIC NEWS APP">STATIC NEWS APP</h2>
-    </div>
+    <>
+      <div className="content">
+        <h2 className="text" data-text="<====Warning====>">
+        ==== Warning ====
+        </h2>
+      </div>
+      <div
+        className="content"
+        style={{ marginBottom: -"70px", marginTop: "-36px" }}
+      >
+        <h2 className="text" data-text="This is a Demo Project">
+          This is a Demo Project
+        </h2>
+      </div>
+    </>
   );
 };
 
